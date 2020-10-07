ce618a7ea2ddb607841ff4418527613f7dfbf250
Ana Betancourt <anaa.betancourt13@gmail.com>
2020-10-07 06:32:50 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -17,6 +17,12 @@ attr_accessor :name, :yrs_experience
     Meal.new(self, customer, total, tip)
   end
   
+  def meals
+    Meal.all.select do |meal|
+      meal.waiter == self 
+    end
+  end
+  
   
   
   
